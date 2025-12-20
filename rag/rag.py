2ede011996c6ccf58e047bea922234fce597e250
Deterraleon <ivolohov95@gmail.com>
2025-12-20 12:10:09 +0500

    finnished up the rag

--- rag/rag.py
+++ rag/rag.py
@@ -1,23 +1,25 @@
 from langchain.agents import create_agent
 from data import database as db
 import os
+from dotenv import load_dotenv
 from dataclasses import dataclass
 from langchain_openai import ChatOpenAI
 from langchain.chat_models import init_chat_model
 from langchain.tools import tool, ToolRuntime
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain.agents.structured_output import ToolStrategy
-os.environ["OPENAI_API_KEY"] = "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"
 @dataclass
 class ResponseFormat:
     """Response schema for the agent."""
     response:str
-    paths:list[str]|str
+    paths:list[str]
 class RAG:
     def __init__(self):
+        load_dotenv()
+        API_KEY = os.getenv("LLM_KEY")
         self.model = ChatOpenAI(
             model="nex-agi/deepseek-v3.1-nex-n1:free",  # OpenRouter model ID
-            openai_api_key="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",  # Your OpenRouter API key
+            openai_api_key=API_KEY,  # Your OpenRouter API key
             openai_api_base="https://openrouter.ai/api/v1",  # OpenRouter endpoint
             temperature=0.3,
             max_tokens=2048

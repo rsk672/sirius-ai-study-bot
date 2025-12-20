4486b297a6d0189c3dda902b5189d8d77db6cfc9
Deterraleon <ivolohov95@gmail.com>
2025-12-20 19:20:04 +0500

    blablabla

--- rag/rag.py
+++ rag/rag.py
@@ -14,6 +14,9 @@ class ResponseFormat:
     response:str
     paths:list[str]
     ratings:list[float]
+
+
+
 class RAG:
     def __init__(self):
         load_dotenv()
@@ -27,9 +30,11 @@ class RAG:
         )
         self.agent = create_agent(
             model=self.model,
+            tools=[self.get_relevant_conspect],
             system_prompt="You are a helpful assistant, " \
             "the user has submited some conspects on various topic. Provide information on the topics that the user has asked," \
-            "you will be provided with the path to the most relevant ones as well as their contents." \
+            "you have a tool called get_relevant_conspect that will provide you with the path to the most relevant conspects as well as their contents," \
+            " at the beggining of the file the user id and the label you'll need for the tool will be provided." \
             "In your response always rely on the conspects for information." \
             "After your response write the paths to the conspects you used for answering the user's question." \
             "Don't repeat the same path twice, if the information in the file wasn't used, don't print out its path." \
@@ -43,12 +48,19 @@ class RAG:
         )
         self.database = db.Database()
     
-    def query(self, text:str, chat_id:int, label:str=None):
-        #2 step RAG
-        #можно потом сделать более сложную архитектуру
-        datas = self.database.get(text=text, chat_id=chat_id, label=label)
-        serialized = "\n\n".join(
+    @tool
+    def get_relevant_conspect(self, user_input:str, chat_id:int, label:str='None')->str:
+        """Finds the relevant conspect to a user input"""
+        print(user_input, chat_id, label)
+        datas = self.database.get(text=user_input, chat_id=chat_id, label=label)
+        output = "\n\n".join(
             (f"Path: {data.path}\nContent: {data.text}") for data in datas
         )
-        response = self.agent.invoke({"messages": [{"role": "user", "content": text + "\n\n" + serialized}]})
+        return output
+
+    def query(self, text:str, chat_id:int, label:str='None'):
+        #agenic RAG
+        print("start")
+        response = self.agent.invoke({"messages": 
+                    [{"role": "user", "content": f"chat_id:{chat_id}, label:{label}\n\n{text}"}]})
         return response['structured_response']

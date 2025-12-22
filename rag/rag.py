c6ab4791fdf20578aa679f249d8e6b3593d19f83
Alexey K <Lexeikrav09@yandex.ru>
2025-12-22 12:13:42 +0300

    rag.py пока без промпта

--- rag/rag.py
+++ rag/rag.py
@@ -24,21 +24,10 @@ class RAG:
             max_tokens=512,
             api_key=API_KEY
         )
+        
         self.agent = create_agent(
             model=self.model,
-            system_prompt="You are a helpful assistant, " \
-            "the user has submited some conspects on various topic. Provide information on the topics that the user has asked," \
-            "you will be provided with the path to the most relevant ones as well as their contents." \
-            "In your response always rely on the conspects for information." \
-            "After your response write the paths to the conspects you used for answering the user's question." \
-            "Don't reference the file paths or names as they are meaningless to the user." \
-            "Don't repeat the same path twice, if the information in the file wasn't used, don't print out its path." \
-            "Only write a path if you think the user would need to see that file to understand the explanation." \
-            "You can make derivations based on the conspects and also to use your own knowledge," \
-            " but ALWAYS and i do mean ALWAYS specify when you are doing so." \
-            "If no conspects were referenced then in the paths write 'None'." \
-            "After your response write a float from 0 to 1 that is related to how much each conspect was used," \
-            " each number should corespond to the path written previously.",
+            system_prompt=
             response_format=ToolStrategy(ResponseFormat),
         )
         self.database = db.Database()

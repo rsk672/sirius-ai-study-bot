d0d7a3fe71d47188456a26acae3ec72025ed6da2
Alexey K <Lexeikrav09@yandex.ru>
2025-12-25 12:05:42 +0300

    Подтянул ocr.py

--- OCR/ocr.py
+++ OCR/ocr.py
@@ -10,6 +10,25 @@ def encode_file(file_path):
     file_content = fid.read()
   return base64.b64encode(file_content).decode("utf-8")
 
+async def PDFToText(path:str)->str:
+    AUTH_KEY = os.getenv("AUTH_KEY")
+    FOLDER_ID = os.getenv("FOLDER_ID")
+    client = httpx.AsyncClient()
+    data = {"mimeType": "application/pdf",
+            "languageCodes": ["ru","en"],
+            "model": "handwritten",
+            "content": encode_file(path)}
+
+    url = "https://ocr.api.cloud.yandex.net/ocr/v1/recognizeText"
+
+    headers= {"Content-Type": "application/json",
+            "Authorization": "Bearer {:s}".format(AUTH_KEY),
+            "x-folder-id": FOLDER_ID,
+            "x-data-logging-enabled": "true"}
+    
+    resp = await client.post(url, headers=headers, data=json.dumps(data))
+    print(resp.json())
+    return resp.json()["result"]["textAnnotation"]["fullText"]
 
 async def ImageToText(path:str)->str:
     AUTH_KEY = os.getenv("AUTH_KEY")
@@ -28,4 +47,4 @@ async def ImageToText(path:str)->str:
             "x-data-logging-enabled": "true"}
     
     resp = await client.post(url, headers=headers, data=json.dumps(data))
-    return resp.json()["result"]["textAnnotation"]["fullText"]
+    return resp.json()["result"]["textAnnotation"]["fullText"].replace('<unk>', '')
